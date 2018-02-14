911d1b3eb7732bf463abb5f57441ee065de8c880
Tom Squillace <tomsquillace@gmail.com>
2018-02-14 00:30:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ function add(a, b){
   return a + b
 }
 function subtract(a, b){
-  return a + b
+  return a - b
+}
+function multiply(a, b){
+  return a * b
+}
+function divide(a, b){
+  return a / b
 }
